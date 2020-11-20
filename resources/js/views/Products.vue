f6d2ba98d287f2ed2f2466dd1a27965405012c23
princedacy <princedacy@gmail.com>
2020-11-20 11:27:46 +0200

    start products CRUD

--- resources/js/views/Products.vue
+++ resources/js/views/Products.vue
@@ -1,111 +1,282 @@
 <template>
-  <a-table :data-source="data" :columns="columns">
-    <div
-      slot="filterDropdown"
-      slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }"
-      style="padding: 8px"
-    >
-      <a-input
-        v-ant-ref="c => (searchInput = c)"
-        :placeholder="`Search ${column.dataIndex}`"
-        :value="selectedKeys[0]"
-        style="width: 188px; margin-bottom: 8px; display: block;"
-        @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
-        @pressEnter="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
-      />
-      <a-button
-        type="primary"
-        icon="search"
-        size="small"
-        style="width: 90px; margin-right: 8px"
-        @click="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
+  <div>
+    <a-button class="editable-add-btn" @click="showModal" type="primary">
+      Add
+    </a-button>
+    <a-table :data-source="data" :columns="columns">
+      <div
+        slot="filterDropdown"
+        slot-scope="{
+          setSelectedKeys,
+          selectedKeys,
+          confirm,
+          clearFilters,
+          column,
+        }"
+        style="padding: 8px"
       >
-        Search
-      </a-button>
-      <a-button size="small" style="width: 90px" @click="() => handleReset(clearFilters)">
-        Reset
-      </a-button>
-    </div>
-    <a-icon
-      slot="filterIcon"
-      slot-scope="filtered"
-      type="search"
-      :style="{ color: filtered ? '#108ee9' : undefined }"
-    />
-    <template slot="customRender" slot-scope="text, record, index, column">
-      <span v-if="searchText && searchedColumn === column.dataIndex">
-        <template
-          v-for="(fragment, i) in text
-            .toString()
-            .split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))"
+        <a-input
+          v-ant-ref="(c) => (searchInput = c)"
+          :placeholder="`Search ${column.dataIndex}`"
+          :value="selectedKeys[0]"
+          style="width: 188px; margin-bottom: 8px; display: block"
+          @change="
+            (e) => setSelectedKeys(e.target.value ? [e.target.value] : [])
+          "
+          @pressEnter="
+            () => handleSearch(selectedKeys, confirm, column.dataIndex)
+          "
+        />
+        <a-button
+          type="primary"
+          icon="search"
+          size="small"
+          style="width: 90px; margin-right: 8px"
+          @click="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
         >
-          <mark
-            v-if="fragment.toLowerCase() === searchText.toLowerCase()"
-            :key="i"
-            class="highlight"
-            >{{ fragment }}</mark
+          Search
+        </a-button>
+        <a-button
+          size="small"
+          style="width: 90px"
+          @click="() => handleReset(clearFilters)"
+        >
+          Reset
+        </a-button>
+      </div>
+      <a-icon
+        slot="filterIcon"
+        slot-scope="filtered"
+        type="search"
+        :style="{ color: filtered ? '#108ee9' : undefined }"
+      />
+      <template slot="customRender" slot-scope="text, record, index, column">
+        <span v-if="searchText && searchedColumn === column.dataIndex">
+          <template
+            v-for="(fragment, i) in text
+              .toString()
+              .split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))"
           >
-          <template v-else>{{ fragment }}</template>
+            <mark
+              v-if="fragment.toLowerCase() === searchText.toLowerCase()"
+              :key="i"
+              class="highlight"
+              >{{ fragment }}</mark
+            >
+            <template v-else>{{ fragment }}</template>
+          </template>
+        </span>
+        <template v-else>
+          {{ text }}
         </template>
-      </span>
-      <template v-else>
-        {{ text }}
       </template>
-    </template>
-  </a-table>
+    </a-table>
+    <!-- Create product modal -->
+    <a-modal
+      title="New product"
+      :visible="visible"
+      :confirm-loading="confirmLoading"
+      @ok="handleOk"
+      @cancel="handleCancel"
+    >
+      <a-form :form="form">
+        <a-form-item
+          :label-col="formItemLayout.labelCol"
+          :wrapper-col="formItemLayout.wrapperCol"
+          label="Name"
+        >
+          <a-input
+            v-decorator="[
+              'name',
+              {
+                rules: [
+                  { required: true, message: 'Please input the product name' },
+                ],
+              },
+            ]"
+            placeholder="Product name"
+          />
+        </a-form-item>
+        <a-form-item
+          :label-col="formItemLayout.labelCol"
+          :wrapper-col="formItemLayout.wrapperCol"
+          label="Category"
+        >
+          <a-input
+            v-decorator="[
+              'category',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input the category',
+                  },
+                ],
+              },
+            ]"
+            placeholder="Category"
+          />
+        </a-form-item>
+        <a-form-item
+          :label-col="formItemLayout.labelCol"
+          :wrapper-col="formItemLayout.wrapperCol"
+          label="Manufacturer"
+        >
+          <a-input
+            v-decorator="[
+              'manufacturer',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input the manufacturer',
+                  },
+                ],
+              },
+            ]"
+            placeholder="Manufacturer ltd"
+          />
+        </a-form-item>
+        <a-form-item
+          :label-col="formItemLayout.labelCol"
+          :wrapper-col="formItemLayout.wrapperCol"
+          label="Purchase price"
+        >
+          <a-input
+            v-decorator="[
+              'Purchase price',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input the purchase price',
+                  },
+                ],
+              },
+            ]"
+            placeholder="0 RWF"
+          />
+        </a-form-item>
+        <a-form-item
+          :label-col="formItemLayout.labelCol"
+          :wrapper-col="formItemLayout.wrapperCol"
+          label="Unit price"
+        >
+          <a-input
+            v-decorator="[
+              'Unit price',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input the unit price',
+                  },
+                ],
+              },
+            ]"
+            placeholder="0 RWF"
+          />
+        </a-form-item>
+        <a-form-item
+          :label-col="formItemLayout.labelCol"
+          :wrapper-col="formItemLayout.wrapperCol"
+          label="Weight"
+        >
+          <a-input
+            v-decorator="[
+              'Weight',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input the weight',
+                  },
+                ],
+              },
+            ]"
+            placeholder="1kg / 1l"
+          />
+        </a-form-item>
+        <a-form-item
+          :label-col="formItemLayout.labelCol"
+          :wrapper-col="formItemLayout.wrapperCol"
+          label="Quantity"
+        >
+          <a-input
+            v-decorator="[
+              'Quantity',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input the quantity',
+                  },
+                ],
+              },
+            ]"
+            placeholder="0"
+          />
+        </a-form-item>
+      </a-form>
+    </a-modal>
+  </div>
 </template>
 
 <script>
 const data = [
   {
-    key: '1',
-    name: 'John Brown',
+    key: "1",
+    name: "John Brown",
     age: 32,
-    address: 'New York No. 1 Lake Park',
+    address: "New York No. 1 Lake Park",
   },
   {
-    key: '2',
-    name: 'Joe Black',
+    key: "2",
+    name: "Joe Black",
     age: 42,
-    address: 'London No. 1 Lake Park',
+    address: "London No. 1 Lake Park",
   },
   {
-    key: '3',
-    name: 'Jim Green',
+    key: "3",
+    name: "Jim Green",
     age: 32,
-    address: 'Sidney No. 1 Lake Park',
+    address: "Sidney No. 1 Lake Park",
   },
   {
-    key: '4',
-    name: 'Jim Red',
+    key: "4",
+    name: "Jim Red",
     age: 32,
-    address: 'London No. 2 Lake Park',
+    address: "London No. 2 Lake Park",
   },
 ];
-
+const formItemLayout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 12 },
+};
+const formTailLayout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 8, offset: 4 },
+};
 export default {
   data() {
     return {
       data,
-      searchText: '',
+      searchText: "",
       searchInput: null,
-      searchedColumn: '',
+      searchedColumn: "",
       columns: [
         {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
+          title: "Name",
+          dataIndex: "name",
+          key: "name",
           scopedSlots: {
-            filterDropdown: 'filterDropdown',
-            filterIcon: 'filterIcon',
-            customRender: 'customRender',
+            filterDropdown: "filterDropdown",
+            filterIcon: "filterIcon",
+            customRender: "customRender",
           },
           onFilter: (value, record) =>
-            record.name
-              .toString()
-              .toLowerCase()
-              .includes(value.toLowerCase()),
-          onFilterDropdownVisibleChange: visible => {
+            record.name.toString().toLowerCase().includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: (visible) => {
             if (visible) {
               setTimeout(() => {
                 this.searchInput.focus();
@@ -114,20 +285,17 @@ export default {
           },
         },
         {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
+          title: "Category",
+          dataIndex: "category",
+          key: "category",
           scopedSlots: {
-            filterDropdown: 'filterDropdown',
-            filterIcon: 'filterIcon',
-            customRender: 'customRender',
+            filterDropdown: "filterDropdown",
+            filterIcon: "filterIcon",
+            customRender: "customRender",
           },
           onFilter: (value, record) =>
-            record.age
-              .toString()
-              .toLowerCase()
-              .includes(value.toLowerCase()),
-          onFilterDropdownVisibleChange: visible => {
+            record.age.toString().toLowerCase().includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: (visible) => {
             if (visible) {
               setTimeout(() => {
                 this.searchInput.focus();
@@ -136,20 +304,108 @@ export default {
           },
         },
         {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
+          title: "Manufacturer",
+          dataIndex: "manufacturer",
+          key: "manufacturer",
           scopedSlots: {
-            filterDropdown: 'filterDropdown',
-            filterIcon: 'filterIcon',
-            customRender: 'customRender',
+            filterDropdown: "filterDropdown",
+            filterIcon: "filterIcon",
+            customRender: "customRender",
           },
           onFilter: (value, record) =>
             record.address
               .toString()
               .toLowerCase()
               .includes(value.toLowerCase()),
-          onFilterDropdownVisibleChange: visible => {
+          onFilterDropdownVisibleChange: (visible) => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: "Purchase price",
+          dataIndex: "purchase price",
+          key: "purchase price",
+          scopedSlots: {
+            filterDropdown: "filterDropdown",
+            filterIcon: "filterIcon",
+            customRender: "customRender",
+          },
+          onFilter: (value, record) =>
+            record.address
+              .toString()
+              .toLowerCase()
+              .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: (visible) => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: "Unit price",
+          dataIndex: "unit price",
+          key: "unit price",
+          scopedSlots: {
+            filterDropdown: "filterDropdown",
+            filterIcon: "filterIcon",
+            customRender: "customRender",
+          },
+          onFilter: (value, record) =>
+            record.address
+              .toString()
+              .toLowerCase()
+              .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: (visible) => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: "Weight",
+          dataIndex: "weight",
+          key: "weight",
+          scopedSlots: {
+            filterDropdown: "filterDropdown",
+            filterIcon: "filterIcon",
+            customRender: "customRender",
+          },
+          onFilter: (value, record) =>
+            record.address
+              .toString()
+              .toLowerCase()
+              .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: (visible) => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: "Quantity",
+          dataIndex: "quantity",
+          key: "quantity",
+          scopedSlots: {
+            filterDropdown: "filterDropdown",
+            filterIcon: "filterIcon",
+            customRender: "customRender",
+          },
+          onFilter: (value, record) =>
+            record.address
+              .toString()
+              .toLowerCase()
+              .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: (visible) => {
             if (visible) {
               setTimeout(() => {
                 this.searchInput.focus();
@@ -158,6 +414,13 @@ export default {
           },
         },
       ],
+      ModalText: "Content of the modal",
+      visible: false,
+      confirmLoading: false,
+      checkNick: false,
+      formItemLayout,
+      formTailLayout,
+      form: this.$form.createForm(this, { name: "dynamic_rule" }),
     };
   },
   methods: {
@@ -169,14 +432,45 @@ export default {
 
     handleReset(clearFilters) {
       clearFilters();
-      this.searchText = '';
+      this.searchText = "";
+    },
+    showModal() {
+      this.visible = true;
+    },
+    handleOk(e) {
+      this.ModalText = "The modal will be closed after two seconds";
+      this.confirmLoading = true;
+      setTimeout(() => {
+        this.visible = false;
+        this.confirmLoading = false;
+      }, 2000);
+    },
+    handleCancel(e) {
+      console.log("Clicked cancel button");
+      this.visible = false;
+    },
+    check() {
+      this.form.validateFields((err) => {
+        if (!err) {
+          console.info("success");
+        }
+      });
+    },
+    handleChange(e) {
+      this.checkNick = e.target.checked;
+      this.$nextTick(() => {
+        this.form.validateFields(["nickname"], { force: true });
+      });
     },
   },
 };
 </script>
-<style scoped>
+<style lang='scss'>
 .highlight {
   background-color: rgb(255, 192, 105);
   padding: 0px;
+}
+.ant-modal {
+  top: 55px !important;
 }
 </style>
